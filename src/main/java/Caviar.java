ea5f526caafe85002322425d6d54da6f1b79b916
wilsonsfh <wilsonsoon2010@hotmail.com>
2025-02-21 16:38:36 +0800

    Level-1

---
+++ src/main/java/Caviar.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Caviar {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Caviar\n" +
+                           "What can I do for you?\n" +
+                           "______________________\n" +
+                           "Bye. Hope to see you again soon!");
+    }
+}
