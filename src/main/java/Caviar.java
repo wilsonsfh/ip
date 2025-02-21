2c88daa6482159d9ee7e8f04e9065fab6fffb7e4
wilsonsfh <wilsonsoon2010@hotmail.com>
2025-02-21 19:53:03 +0800

    Level-1 Add support for echo

--- src/main/java/Caviar.java
+++ src/main/java/Caviar.java
@@ -2,9 +2,33 @@ import java.util.Scanner;
 
 public class Caviar {
     public static void main(String[] args) {
+        String inData = "";
+        Boolean toggle = true;
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Caviar\n" +
-                           "What can I do for you?\n" +
-                           "______________________\n" +
-                           "Bye. Hope to see you again soon!");
+                "What can I do for you?\n" +
+                "______________________\n");
+        Scanner scan = new Scanner(System.in);
+        while(toggle) {
+            inData = scan.nextLine();
+            if (!inData.equals("bye")) {
+
+                if (inData.equals("")) {
+                    System.out.println("no input, type something roe.");
+                } else {
+                    System.out.println(inData + " roe.");
+                }
+            }
+            else {
+                toggle = false;
+                System.out.println("Roe. Hope to see you again soon!");
+            }
+
+
+        }
     }
 }
