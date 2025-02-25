f9a282b6e91426c275650ace47b1c02444df6453
wilsonsfh <wilsonsoon2010@hotmail.com>
2025-02-25 19:42:13 +0800

    Add find functionality and fix parser logic

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -1,10 +1,15 @@
 package caviar.parser;
 
+import caviar.task.Task;
+import caviar.task.Deadline;
+import caviar.task.Todo;
+import caviar.task.Event;
 import caviar.command.TaskList;
 import caviar.exception.CaviarException;
 import caviar.storage.Storage;
 import caviar.ui.Ui;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,5 +30,14 @@ class ParserTest {
                 Parser.parseAndExecute("unknownCommand", taskList, ui, storage)
         );
         assertTrue(exception.getMessage().contains("roe..!!"), "Should throw an error for unknown commands.");
+    }
+
+    @Test
+    void testParseFind() throws CaviarException {
+        taskList.addTask(new Todo("read book"));
+        taskList.addTask(new Deadline("return book", "2025-02-25"));
+
+        List<Task> results = taskList.findTasks("book");
+        assertEquals(2, results.size(), "Both tasks should be found.");
     }
 }
--- src/test/java/TaskListTest.java
+++ src/test/java/TaskListTest.java
@@ -1,9 +1,11 @@
 package caviar.command;
 
+import caviar.task.Task;
 import caviar.task.Todo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 class TaskListTest {
     private TaskList taskList;
@@ -11,6 +13,17 @@ class TaskListTest {
     @BeforeEach
     void setUp() {
         taskList = new TaskList();
+    }
+
+    @Test
+    void testFindTasks() {
+        taskList.getTasks().add(new Todo("Read book"));
+        taskList.getTasks().add(new Todo("Return book to library"));
+        taskList.getTasks().add(new Todo("Go for a run"));
+        List<Task> results = taskList.findTasks("book");
+        assertEquals(2, results.size(), "Should return 2 matching tasks.");
+        assertEquals("Read book", results.get(0).getDescription());
+        assertEquals("Return book to library", results.get(1).getDescription());
     }
 
     @Test
